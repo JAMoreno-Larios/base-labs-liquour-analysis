0ff070a12a495d583c904b22937df65262457c13
José Agustín Moreno Larios <agustin90m@gmail.com>
2026-05-06 14:56:17 -0600

    Completed first draft for COGS calculation

--- src/analysis.py
+++ src/analysis.py
@@ -11,11 +11,12 @@ J. A. Moreno
 import sqlalchemy as sa
 from config import Config
 import pandas as pd
+import numpy as np
 
 # Create engine
 engine = sa.create_engine(Config.SQLALCHEMY_DATABASE_URI, echo=True)
 
-def calculate_cogs_per_brand():
+def calculate_cogs_per_brand() -> pd.DataFrame:
     """
     Calculate the Cost Of Sold Goods based on inventory and purchases.
 
@@ -29,19 +30,12 @@ def calculate_cogs_per_brand():
     """
     
     # Get initial inventory value per brand
-    initial_inv = pd.read_sql("""
+    initial_inventory = pd.read_sql("""
         SELECT Brand, SUM(onHand * Price) as beg_inv_value
         FROM BegInvDec
         GROUP BY Brand
     """, engine)
-
-    # Calculate the purchases done per brand
-    purchases = pd.read_sql("""
-        SELECT Brand, SUM(onHand * Price) as beg_inv_value
-        FROM BegInvDec
-        GROUP BY Brand
-    """, engine)
-
+    
     # Calculate the ending inventory value per brand
     ending_inventory = pd.read_sql("""
         SELECT Brand, SUM(onHand * Price) as end_inv_value
@@ -49,6 +43,60 @@ def calculate_cogs_per_brand():
         GROUP BY Brand
     """, engine)
 
+    # Calculate the purchases done per brand
+    purchases = pd.read_sql("""
+        SELECT Brand,
+        SUM(Dollars) as total_purchases
+        FROM PurchasesDec
+        GROUP BY Brand
+    """, engine)
+
     # Calculate the freight cost per brand
+    # Total freight cost across all invoices
+    total_freight = pd.read_sql(
+        "SELECT SUM(Freight) AS total_freight FROM VendorInvoicesDec",
+        engine).iloc[0]["total_freight"]
     # We will take the brand's proportional freight share from the total
     # freight costs
+    freight_allocation = purchases.copy()
+    total_purchases = freight_allocation["total_purchases"].sum()
+    # Figure out if we did purchase items for a given brand, if so,
+    # calculate the brand's proportional freight cost.
+    freight_allocation["freight_allocation"] = np.where(
+        total_purchases > 0,
+        (freight_allocation["total_purchases"] / total_purchases) * total_freight,
+        0.0
+    )
+
+    # Merge dataframes, use union of keys for each pair of frames
+    # Merge initial with ending
+    cogs = initial_inventory.merge(ending_inventory, how="outer", on="Brand")
+    # Merge with purchases
+    cogs = cogs.merge(purchases, how="outer", on="Brand")
+    # Merge with freight allocation
+    cogs = cogs.merge(
+        freight_allocation[["Brand", "freight_allocation"]],
+        how="outer",
+        on="Brand"
+    )
+
+    # If there are NaNs, fill with zeros
+    cogs = cogs.fillna(0)
+
+    # Calculate the Cost of Goods Sold
+    cogs["cogs"] = (
+        cogs["beg_inv_value"]
+        + cogs["total_purchases"]
+        + cogs["freight_allocation"]
+        - cogs["end_inv_value"]
+    )
+
+    # Return completed dataframe
+    return cogs[["Brand", "beg_inv_value", "total_purchases",
+                 "freight_allocation", "end_inv_value",
+                 "cogs"]]
+
+if __name__ == "__main__":
+    # Run
+    cogs = calculate_cogs_per_brand()
+    print(cogs.sort_values("cogs", ascending=False).head(20).to_string())
