31dca1fc5cc477dfe38c048c9cd5d67486ee27a3
José Agustín Moreno Larios <agustin90m@gmail.com>
2026-05-06 19:09:36 -0600

    Added vendor-based profit and margin calculations

--- src/analysis.py
+++ src/analysis.py
@@ -172,6 +172,39 @@ def calculate_brand_profits_margins(cogs_brand: pd.DataFrame) -> pd.DataFrame:
     return summary
 
 
+def calculate_vendor_profits_margins(cogs_vendor: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculates the profits and margins in a per-vendor basis using
+    a previously calculated COGS.
+    The Profit is calculated as Revenue - COGS,
+    the margins as (Revenue - COGS) / Revenue
+    """
+
+    # Aggregate revenue by brand
+    revenue = pd.read_sql("""
+        SELECT
+            VendorNo as VendorNumber,
+            SUM(SalesDollars) AS total_revenue,
+            SUM(SalesQuantity) AS total_units
+            FROM SalesDec
+            GROUP BY VendorNumber
+    """, engine)
+
+    # Merge revenue and COGS into a new data frame, do calculations
+    summary = revenue.merge(cogs_vendor, how="left", on="VendorNumber")
+    summary["cogs"] = summary["cogs"].fillna(0)  # Replace NaNs with zero
+
+    # Calculate profit and margin
+    summary["profit"] = summary["total_revenue"] - summary["cogs"]
+    summary["margin"] = np.where(
+        summary["total_revenue"] > 0,
+        summary["profit"] / summary["total_revenue"] * 100,
+        np.nan
+    )
+    # Return final results
+    return summary
+
+
 if __name__ == "__main__":
     # Run
     # cogs_brand = calculate_cogs_per_brand()
@@ -192,3 +225,13 @@ if __name__ == "__main__":
     cogs_vendor = calculate_cogs_per_vendor()
     print("===COGS PER VENDOR===")
     print(cogs_vendor.sort_values("cogs", ascending=False).head(5).to_string(index=False))
+    print("===PROFITS===")
+    summary = calculate_vendor_profits_margins(cogs_vendor)
+    print(summary.nlargest(10, "profit").to_string(index=False))
+    print("===MARGINS===")
+    print(summary.nlargest(10, "margin").to_string(index=False))
+    print("===LOSING VENDORS===")
+    losing_vendors = summary[summary["profit"] < 0].sort_values("profit")
+    print(losing_vendors[["VendorNumber", "total_revenue",
+                        "cogs", "profit", "margin"]]
+          .to_string(index=False))
